27edb0f263696f1e50a8bf069e15c074a7aaf5a5
mrunal <mrunalbhoir100@gmail.com>
2022-08-31 21:35:59 +0530

    Prevent duplicate contact

--- AddressBook.js
+++ AddressBook.js
@@ -122,4 +122,12 @@ let count=()=>{
     console.log(addressBookArray.length);
 }
 count();
+let duplicate=(firstName,city,phoneNo)=>{
+    if(addressBookArray.some(contacts=>contacts.firstName==firstName)){
+        console.log("contact already exist");
+    }
+    else
+    addressBookArray.push(new Contact(firstName,city,phoneNo));
+}
+duplicate("Mrunal1");
 
